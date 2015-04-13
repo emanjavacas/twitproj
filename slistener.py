b9e5ad123b54e714c05e45f4d90a9a3907b8023e
emanjavacas <enrique.manjavacas@gmail.com>
2015-04-13 16:15:51 +0200

    backup

--- slistener.py
+++ slistener.py
@@ -2,22 +2,13 @@ from tweepy import StreamListener
 from collections import defaultdict
 import time, json, codecs
 from ldig import ldig
-import langid
+import langid, cld
+import pymongo
 
-det = ldig.ldig('ldig/models/model.latin')
-detectors = {
-    'langid_guess' : lambda x: langid.classify(x)[0],
-    'ldig_guess' : lambda x: det.detect('model.latin', x)[1]
-    }
+conn = pymongo.MongoClient()
+db = conn['twitdb']
+coll = db['tweets']
 
-#not-tested
-# "cld" : lambda x: cld.detect(x.encode('utf-8'))[1]
-#('SPANISH', 'es', True, 134, [('SPANISH', 'es', 100, 84.11214953271028)])
-
-def handle_lang(tweet):
-    ''' return language guesses according the detectors dictionary '''
-    return {k : v(tweet) for k, v in detectors.items()}
-    
 # create a subclass of StreamListener with desired settings
 class SListener(StreamListener):
     ''' creates a stream '''
@@ -60,14 +51,19 @@ class SListener(StreamListener):
             f.write(str(status_id) + '\n')
         return
     
+    # def on_status(self, status):
+    #     with codecs.open(self.output, 'a', 'utf-8') as f:
+    #         f.write(self.handle_tweet(status) + '\n')
+    #     print self.handle_tweet(status).encode('utf-8')
+    #     self.counter += 1
+    #     if self.counter >= 5000:
+    #         self.update_outputfile()
+    #         self.counter = 0
+    #         return False
+    #     return
+
     def on_status(self, status):
-        with codecs.open(self.output, 'a', 'utf-8') as f:
-            f.write(self.handle_tweet(status) + '\n')
-        self.counter += 1
-        if self.counter >= 5000:
-            self.update_outputfile()
-            self.counter = 0
-            return False
+        coll.insert(self.handle_tweet(status))
         return
 
     def update_outputfile(self):
@@ -83,20 +79,27 @@ class SListener(StreamListener):
         my_tweet = filter_json(self.tweet_keys, tweet)
         # add lang guesses
         guesses = handle_lang(tweet['text'])
+        guesses['twitter_guess'] = my_tweet['lang']
+        del my_tweet['lang']
+        my_tweet['langs'] = {}
         for k, v in guesses.items():
-            my_tweet[k] = v
-        # process tweet
-        my_tweet['text'] = unicode(my_tweet['text']).encode('utf-8')
+            my_tweet['langs'][k] = v
+        # process tweet (not necessary)
+        # my_tweet['text'] = unicode(my_tweet['text']).encode('utf-8')
+        my_tweet['city'] = self.fileprefix
         # monitoring
-        print "%s\nTwitter: [%s], LDIG: [%s], langid: [%s]" \
-            % (my_tweet['text'].decode('utf-8'), my_tweet['lang'], \
-            my_tweet['langid_guess'], my_tweet['ldig_guess'])
+        print "%s\n%s: [%s], %s: [%s], %s: [%s], %s: [%s]" \
+            % tuple([my_tweet['text'].encode('utf-8')] +
+                    [i.encode('utf-8') for tpl in my_tweet['langs'].items() for i in tpl])
         print my_tweet['coordinates']
         print "***"
-        return json.dumps(my_tweet)
+        # return json.dumps(my_tweet)
+        return my_tweet
 
 def filter_json(keys, json_obj):
-    '''   closure on _filter_json applying SListener.keys    '''
+    '''
+    closure on _filter_json applying SListener.keys
+    '''
     acc = defaultdict(defaultdict)
     _filter_json(keys, json_obj, acc)
     return acc
@@ -112,3 +115,15 @@ def _filter_json(keys, json_obj, acc):
             acc[k] = json_obj[k]
         else: #dict
             _filter_json(v, json_obj[k], acc[k])
+
+det = ldig.ldig('ldig/models/model.latin')
+detectors = {
+    'langid_guess' : lambda x: langid.classify(x)[0],
+    'ldig_guess' : lambda x:
+        det.detect('model.latin', x)[1],
+    'cld_guess' : lambda x: cld.detect(x.encode('utf-8'))[1]
+    }
+    
+def handle_lang(tweet):
+    ''' return language guesses according the detectors dictionary '''
+    return {k : v(tweet) for k, v in detectors.items()}
